f5e21d6df23b1473080522791668dace96c2faa8
rkarikari <115777368+rkarikari@users.noreply.github.com>
2024-08-03 18:00:36 +0000

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -139,7 +139,7 @@ INPUT_TYPE="$INPUT_TYPE"
 
 MLATSERVER="192.168.8.233:31090"
 TARGET="--net-connector 192.168.8.233,30004,beast_reduce_out,192.168.8.233,64004"
-NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval 0.2 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30154 --net-bo-port 0 --net-ri-port 0 --write-json-every 1"
+NET_OPTIONS="--net-heartbeat 60 --net-ro-size 1280 --net-ro-interval=0.05 --net-ro-interval-beast-reduce=0.12 --net-ro-port 0 --net-sbs-port 0 --net-bi-port 30154 --net-bo-port 0 --net-ri-port 0 --write-json-every 1"
 JSON_OPTIONS="--max-range 450 --json-location-accuracy 2 --range-outline-hours 24"
 EOF
 
